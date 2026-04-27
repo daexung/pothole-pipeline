b291dca8108169d96787e8d55d2c80591dcd6c59
daexung <wneotjd102100@gmail.com>
2026-04-27 11:25:34 +0900

    Feat: extract.py 틀 잡기

---
+++ src/etl/extract.py
@@ -0,0 +1,19 @@
+"""
+S3에 업로드된 블랙박스 영상을 다운로드하고,
+특정 FPS로 프레임 추출한다.
+"""
+
+# 1. 모듈  가져오기
+import os
+import cv2
+import boto3
+
+# 2. 함수 정의
+
+def run_extract():
+
+    pass
+
+
+if __name__ == "__main__" :
+    run_extract()
